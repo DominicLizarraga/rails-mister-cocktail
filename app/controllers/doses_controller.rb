cd02d52c327fd4fca25a2e0babd5f7782783f7fa
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-01-14 17:56:18 -0800

    test push

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,27 @@
+class DosesController < ApplicationController
+
+
+  def create
+    @dose = Dose.new(doses_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.destroy_all
+
+
+puts "creating seeds"
+ingredient_1 = Ingredient.new(name: "lemon")
+ingredient_1.save
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "whit")
+Ingredient.create(name: "piece")
+
+
+
+cocktail_1 = Cocktail.new(name: "limonada_1")
+cocktail_1.save
+
+Dose.create(description: "cube", ingredient: ingredient_1, cocktail_id: cocktail_1 )
+
+
+
+
+
